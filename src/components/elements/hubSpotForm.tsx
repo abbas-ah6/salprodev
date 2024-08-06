c3810295d635df15d276459d26d3ae9830d1592d
Muhammad Abbas <i-muhammadabbas@two.cx>
2024-08-07 00:31:42 +0500

    hubspot form fix

--- src/components/elements/hubSpotForm.tsx
+++ src/components/elements/hubSpotForm.tsx
@@ -1,39 +1,23 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
-const HubSpotFormEmbed: React.FC = () => {
-  const formContainerRef = useRef<HTMLDivElement | null>(null);
-
+export default function HubSpotFormEmbed() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.charset = "utf-8";
-    script.type = "text/javascript";
-    script.src = "//js.hsforms.net/forms/embed/v2.js";
-    script.onload = () => {
-      if (window.hbspt && formContainerRef.current) {
-        window.hbspt.forms.create({
-          region: "na1",
-          portalId: "46996366",
-          formId: "852f55df-d7d9-40e4-97d9-01c5d2f9678d",
-          target: formContainerRef.current,
-        });
-      }
-    };
+    script.src = "https://js.hsforms.net/forms/v2.js";
     document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "46996366",
+          formId: "852f55df-d7d9-40e4-97d9-01c5d2f9678d",
+          target: "#hubspotForm",
+        });
+      }
+    });
   }, []);
 
-  return (
-    <div
-      ref={formContainerRef}
-      className="w-full mt-[48px]"
-      style={{ height: "750px", width: "350px" }}
-    />
-  );
-};
-
-export default HubSpotFormEmbed;
+  return <div id="hubspotForm" className="hubspotForm"></div>;
+}
