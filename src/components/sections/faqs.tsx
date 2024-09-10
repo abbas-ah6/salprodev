148510d993c0a45d70211a72ae9ca517ef796a08
Muhammad Abbas <i-muhammadabbas@two.cx>
2024-09-10 14:24:56 +0500

    fix: faqs structured data/fixed

--- src/components/sections/faqs.tsx
+++ src/components/sections/faqs.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { IFaqItem, IFaqs } from "@/types";
-import { FAQPage } from "schema-dts";
+import { Answer, FAQPage, Question } from "schema-dts";
 
 const FaqCard: React.FC<{
   faq: IFaqItem;
@@ -51,59 +51,68 @@ const Faqs: React.FC<IFaqs> = ({ description, heading, image, faqs }) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
-  const faqsStructuredData: FAQPage = {
-    "@type": "FAQPage",
-    mainEntity: faqs?.map((faq) => ({
-      "@type": "Question",
-      name: faq?.question,
-      acceptedAnswer: {
-        "@type": "Answer",
-        text: faq?.answer,
-      },
-    })),
-  };
+  const faqsStructuredData: FAQPage | undefined =
+    faqs && faqs.length > 0
+      ? {
+          "@type": "FAQPage",
+          mainEntity: faqs?.map((faq) => ({
+            "@type": "Question" as Question["@type"],
+            name: faq?.question,
+            acceptedAnswer: {
+              "@type": "Answer" as Answer["@type"],
+              text: faq?.answer,
+            },
+          })),
+        }
+      : undefined;
 
   return (
-    <section className="flex flex-col w-full bg-white">
-      <div className="flex flex-col lg:px-[30px] lg:py-[60px] py-[36px] px-[24px] w-full container mx-auto">
-        <div className="flex flex-col max-w-[725px] w-full justify-start lg:text-start text-center">
-          <h3 className="mt-4 lg:text-h3 text-h3M leading-small text-center lg:text-start text-brand-purple">
-            {heading || ""}
-          </h3>
-          <p className="text-b18 text-brand-gray leading-medium mt-[24px]">
-            {description || ""}
-          </p>
-        </div>
-        <div className="relative mt-[48px] flex lg:flex-row flex-col lg:justify-between justify-center items-center w-full gap-[42px]">
-          <div className="flex-1 flex flex-col relative">
-            <Image
-              loading="lazy"
-              src={image?.src || ""}
-              alt={image?.alt || "Project Image"}
-              width={520}
-              height={470}
-              className="w-full max-w-full max-h-[470px] object-contain object-center transition-all duration-300 ease-in-out"
-            />
+    <>
+      {faqsStructuredData && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(faqsStructuredData),
+          }}
+        />
+      )}
+      <section className="flex flex-col w-full bg-white">
+        <div className="flex flex-col lg:px-[30px] lg:py-[60px] py-[36px] px-[24px] w-full container mx-auto">
+          <div className="flex flex-col max-w-[725px] w-full justify-start lg:text-start text-center">
+            <h3 className="mt-4 lg:text-h3 text-h3M leading-small text-center lg:text-start text-brand-purple">
+              {heading || ""}
+            </h3>
+            <p className="text-b18 text-brand-gray leading-medium mt-[24px]">
+              {description || ""}
+            </p>
           </div>
-          <div className="flex-1 grid lg:grid-cols-1 grid-cols-1 gap-[12px]">
-            {faqs &&
-              faqs.length > 0 &&
-              faqs.map((faq, index) => (
-                <FaqCard
-                  key={index}
-                  faq={faq}
-                  isActive={activeIndex === index}
-                  onClick={() => handleFaqClick(index)}
-                />
-              ))}
+          <div className="relative mt-[48px] flex lg:flex-row flex-col lg:justify-between justify-center items-center w-full gap-[42px]">
+            <div className="flex-1 flex flex-col relative">
+              <Image
+                loading="lazy"
+                src={image?.src || ""}
+                alt={image?.alt || "Project Image"}
+                width={520}
+                height={470}
+                className="w-full max-w-full max-h-[470px] object-contain object-center transition-all duration-300 ease-in-out"
+              />
+            </div>
+            <div className="flex-1 grid lg:grid-cols-1 grid-cols-1 gap-[12px]">
+              {faqs &&
+                faqs.length > 0 &&
+                faqs.map((faq, index) => (
+                  <FaqCard
+                    key={index}
+                    faq={faq}
+                    isActive={activeIndex === index}
+                    onClick={() => handleFaqClick(index)}
+                  />
+                ))}
+            </div>
           </div>
         </div>
-      </div>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: faqsStructuredData }}
-      />
-    </section>
+      </section>
+    </>
   );
 };
 
